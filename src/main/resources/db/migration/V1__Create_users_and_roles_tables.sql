142e92b8c5b1ac9419f5b87d26f3fbca55588ec4
vuong tran <ext.vuong.tran@gxbank.net>
2024-06-30 17:28:02 +0700

    update flyway to accept utf8 on sqlserver 2017

--- src/main/resources/db/migration/V1__Create_users_and_roles_tables.sql
+++ src/main/resources/db/migration/V1__Create_users_and_roles_tables.sql
@@ -13,101 +13,101 @@ drop table if exists users;
 CREATE TABLE access_token
 (
     id            BIGINT IDENTITY (1,1) PRIMARY KEY,
-    created_by    VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL,
+    created_by    NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     created_date  DATETIME,
-    updated_by    VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL,
+    updated_by    NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     updated_date  DATETIME,
     account_id    BIGINT,
     expired       DATETIME,
     refresh_token VARCHAR(600),
     token         VARCHAR(600),
-    user_agent    VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8
+    user_agent    NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS
 );
 
 CREATE TABLE packages
 (
     id             BIGINT IDENTITY (1,1) PRIMARY KEY,
-    created_by     VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL,
+    created_by     NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     created_date   DATETIME,
-    updated_by     VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL,
+    updated_by     NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     updated_date   DATETIME,
-    label          VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8,
-    packaging_name VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8,
+    label          NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS,
+    packaging_name NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS,
     price          NUMERIC(19, 4),
-    uom            VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8,
-    value          VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8
+    uom            NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS,
+    value          NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS
 );
 
 CREATE TABLE provinces
 (
     id            BIGINT IDENTITY (1,1) PRIMARY KEY,
-    created_by    VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL,
+    created_by    NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     created_date  DATETIME,
-    updated_by    VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL,
+    updated_by    NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     updated_date  DATETIME,
-    label         VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8,
-    licenseplates VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8,
-    value         VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8
+    label         NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS,
+    licenseplates NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS,
+    value         NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS
 );
 
 CREATE TABLE remaining_prices
 (
     id            BIGINT IDENTITY (1,1) PRIMARY KEY,
-    created_by    VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL,
+    created_by    NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     created_date  DATETIME,
-    updated_by    VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL,
+    updated_by    NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     updated_date  DATETIME,
-    distance_code VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8,
-    distance_name VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8,
+    distance_code NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS,
+    distance_name NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS,
     time          VARCHAR(255)
 );
 
 CREATE TABLE prices
 (
     id                 BIGINT IDENTITY (1,1) PRIMARY KEY,
-    created_by         VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL,
+    created_by         NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     created_date       DATETIME,
-    updated_by         VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL,
+    updated_by         NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     updated_date       DATETIME,
     max_kg             FLOAT,
     min_kg             FLOAT,
     price_add          BIT,
-    price_code         VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8,
-    price_name         VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8,
-    price_number       VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8,
+    price_code         NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS,
+    price_name         NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS,
+    price_number       NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS,
     remaining_price_id BIGINT FOREIGN KEY REFERENCES remaining_prices (id)
 );
 
 CREATE TABLE roles
 (
     id           BIGINT IDENTITY (1,1) PRIMARY KEY,
-    created_by   VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL,
+    created_by   NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     created_date DATETIME,
-    updated_by   VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL,
+    updated_by   NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     updated_date DATETIME,
-    name         VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL UNIQUE
+    name         NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL UNIQUE
 );
 
 CREATE TABLE transportation_routes
 (
     id                        BIGINT IDENTITY (1,1) PRIMARY KEY,
-    created_by                VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL,
+    created_by                NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     created_date              DATETIME,
-    updated_by                VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL,
+    updated_by                NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     updated_date              DATETIME,
-    distance_name             VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8,
-    label                     VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8,
-    transportation_route_code VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8,
-    value                     VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8,
+    distance_name             NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS,
+    label                     NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS,
+    transportation_route_code NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS,
+    value                     NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS,
     province_id               BIGINT FOREIGN KEY REFERENCES provinces (id)
 );
 
 CREATE TABLE user_token
 (
     id            BIGINT IDENTITY (1,1) PRIMARY KEY,
-    created_by    VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL,
+    created_by    NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     created_date  DATETIME,
-    updated_by    VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL,
+    updated_by    NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     updated_date  DATETIME,
     expired_time  DATETIME,
     refresh_token VARCHAR(500),
@@ -117,12 +117,12 @@ CREATE TABLE user_token
 CREATE TABLE users
 (
     id            BIGINT IDENTITY (1,1) PRIMARY KEY,
-    created_by    VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL,
+    created_by    NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     created_date  DATETIME,
-    updated_by    VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8 NOT NULL,
+    updated_by    NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
     updated_date  DATETIME,
     email         VARCHAR(255) UNIQUE,
-    name          VARCHAR(255) COLLATE Latin1_General_100_CI_AS_SC_UTF8,
+    name          NVARCHAR(255) COLLATE SQL_Latin1_General_CP1_CI_AS,
     password_hash VARCHAR(255) NOT NULL,
     status        VARCHAR(255),
     username      VARCHAR(255) UNIQUE
